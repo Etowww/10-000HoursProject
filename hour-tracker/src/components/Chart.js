44bd154fa21eb6f1ea456884254e295ab81c4577
Etowww <etow.engineering@gmail.com>
2024-05-28 14:42:37 -0700

    Chart Config1

--- hour-tracker/src/components/Chart.js
+++ hour-tracker/src/components/Chart.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
 
 const Chart = ({ data }) => {
-  return (
-    <div>
-      <h2>Hour Chart</h2>
-      <Line data={data} />
-    </div>
-  );
+    return (
+        <div>
+            <h2>Hours Logged</h2>
+            <Bar
+                data={data}
+                options={{
+                    indexAxis: 'x', // Display bars along the x-axis
+                    scales: {
+                        x: {
+                            title: {
+                                display: true,
+                                text: 'Activity',
+                            },
+                        },
+                        y: {
+                            beginAtZero: true, // Start y-axis at 0
+                            title: {
+                                display: true,
+                                text: 'Hours Logged',
+                            },
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
 };
 
 export default Chart;
+
--- hour-tracker/src/components/FrontPage.js
+++ hour-tracker/src/components/FrontPage.js
@@ -2,26 +2,28 @@ import React from 'react';
 import Chart from './Chart';
 
 const FrontPage = () => {
-  const data = {
-    labels: ['Activity 1', 'Activity 2', 'Activity 3', 'Activity 4', 'Activity 5'],
-    datasets: [
-      {
-        label: 'Hours Logged',
-        data: [10, 20, 15, 25, 30], // Sample data for hours logged
-        fill: false,
-        backgroundColor: 'rgba(75,192,192,0.2)',
-        borderColor: 'rgba(75,192,192,1)',
-      },
-    ],
-  };
+    const data = {
+        labels: ['Activity 1', 'Activity 2', 'Activity 3', 'Activity 4', 'Activity 5'],
+        datasets: [
+            {
+                label: 'Hours Logged',
+                data: [10, 20, 15, 25, 30], // Sample data for hours logged
+                backgroundColor: 'rgba(75,192,192,0.2)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderWidth: 1, // Add border width for bars
+            },
+        ],
+    };
 
-  return (
-    <div>
-      <h1>Hour Tracker</h1>
-      <Chart data={data} />
-      {/* Add buttons to add activity and log hours */}
-    </div>
-  );
+    return (
+        <div>
+            <h1>10,000 Hour Project</h1>
+            {/* Add Chart component here */}
+            <Chart data={data} />
+            {/* Add buttons to add activity and log hours */}
+        </div>
+    );
 };
 
 export default FrontPage;
+
